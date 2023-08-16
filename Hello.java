74a874c62e76d02b0ddc245f9520f25b7da314c3
Ron Anthony Sy <142371703+rnnthnysy@users.noreply.github.com>
2023-08-16 12:33:12 +0800

    Create Hello.java

---
+++ Hello.java
@@ -0,0 +1,6 @@
+public class Hello{
+  public void main(String[] args){
+    System.out.println("Hello, Software Engineering Course!");
+    System.out.println("My name is SY, Ron Anthony");
+  }
+}
