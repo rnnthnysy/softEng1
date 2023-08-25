79e8f9601d83c20f50c254899af2dcd7a6b9e46b
Jasmine Ignacio <syronanthony@gmail.com>
2023-08-25 22:11:45 +0800

    updating Hello.java

--- Hello.java
+++ Hello.java
@@ -2,6 +2,6 @@ public class Hello{
   public void main(String[] args){
     System.out.println("Hello, Software Engineering Course!");
     System.out.println("My name is SY, Ron Anthony");
-
+    System.out.println ("Complicated world of SoftDev that is why it pays much!");
   }
 }
